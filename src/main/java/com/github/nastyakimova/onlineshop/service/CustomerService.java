bfd91020c22d33064f1a96c8fc7e93d26df07478
nastyak <nastya.iakimova@gmail.com>
2016-02-19 22:01:14 +0400

    added CustomerService api

---
+++ src/main/java/com/github/nastyakimova/onlineshop/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.github.nastyakimova.onlineshop.service;
+
+import com.github.nastyakimova.onlineshop.entity.Customer;
+
+public interface CustomerService {
+    void createCustomer(Customer customer);
+    Customer getCustomerById(int customerID);
+    void updateCustomer(Customer customer);
+    void deleteCustomer(Customer customer);
+
+}
