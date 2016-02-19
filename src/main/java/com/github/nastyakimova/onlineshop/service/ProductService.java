402e4db3fb7fd321fa96180e7f0fc217143172d5
nastyak <nastya.iakimova@gmail.com>
2016-02-19 22:09:35 +0400

    added ProductService api

---
+++ src/main/java/com/github/nastyakimova/onlineshop/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.github.nastyakimova.onlineshop.service;
+
+import com.github.nastyakimova.onlineshop.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    void createProduct(Product product);
+    Product getProductById(int productID);
+    void deleteProduct(Product product);
+    void updateProduct(Product product);
+    List<Product> getAllProducts();
+}
