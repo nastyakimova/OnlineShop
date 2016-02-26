3ced6e78c266729381ceaeffe6c6f80fc9ba1f0c
nastyak <nastya.iakimova@gmail.com>
2016-02-26 20:55:16 +0400

    added UserService interface and its implementation

--- src/main/java/com/github/nastyakimova/onlineshop/repositories/UserRepository.java
+++ src/main/java/com/github/nastyakimova/onlineshop/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.github.nastyakimova.onlineshop.repositories;
 
 import com.github.nastyakimova.onlineshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findByUsername(String username);
 }
