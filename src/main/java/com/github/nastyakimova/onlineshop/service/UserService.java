d370128abf741d2db3bf683b28d477d679cbd189
nastyak <nastya.iakimova@gmail.com>
2016-02-26 20:54:27 +0400

    updated entities

---
+++ src/main/java/com/github/nastyakimova/onlineshop/service/UserService.java
@@ -0,0 +1,7 @@
+package com.github.nastyakimova.onlineshop.service;
+
+import com.github.nastyakimova.onlineshop.entity.User;
+
+public interface UserService {
+    User getUser(String username);
+}
