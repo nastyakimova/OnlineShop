5f78e4d7b74defe7fb7dbcd2db222c22317de098
Anastasiia <nastya.iakimova@gmail.com>
2016-02-27 18:49:39 +0300

    added spring security

--- src/main/java/com/github/nastyakimova/onlineshop/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.github.nastyakimova.onlineshop.service;
-
-import com.github.nastyakimova.onlineshop.entity.User;
-
-public interface UserService {
-    User getUser(String username);
-}
