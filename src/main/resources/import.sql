5c639a682702e8def4400f7cfcf5e1c88a55b000
nastyak <nastya.iakimova@gmail.com>
2016-03-10 13:57:48 +0400

    fixed bugs

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
-INSERT INTO users(username, enabled, password) VALUES ('admin',TRUE ,'admin');
+INSERT INTO users(username, enabled, password) VALUES ('admin',TRUE ,'$2a$10$/usmqklkr2iQFWg8hKXyf.C8gD6/vZdnSvpSBAvqgXhfC0YIjGJ/C');
+INSERT INTO users(username, enabled, password) VALUES ('user',TRUE ,'$2a$10$0oYdTjFdnfTzWXmVL7Y4BudV5mw0DvrPhOPKJ4tqi4MkVtX8KIlra');
 INSERT INTO authorities(id, authority, username) VALUES ('1','admin','admin');
+INSERT INTO authorities(id, authority, username) VALUES ('2','user','user');
 
